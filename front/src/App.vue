67aa840f8a1f27d9b74a0452f50c6fdcb9b80bb4
razdrayv <boryanspritefanta14@gmail.com>
2023-08-09 21:02:53 +0300

    second commit

---
+++ front/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <HeaderComp/>
+    <RouterView/>
+  </div>
+</template>
+
+<script>
+import HeaderComp from './components/HeaderComp.vue';
+import {RouterView} from 'vue-router'
+  export default {
+    components:{
+      HeaderComp
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ front/src/components/HeaderComp.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <a href="/">Post</a>
+        <a href="http://localhost:5173/">Home</a>
+    </header>
+    <div>
+        <nav>
+            <RouterLink to="/">hello world</RouterLink>
+        </nav>
+        
+    </div>
+</template>
+
+<script>
+    import {RouterLink} from 'vue-router'
+    export default {
+    name:'HeaderComp'
+}
+</script>
+
+<style scoped>
+header{
+    background-color: aqua;
+    height: 50px;
+    width: 100%;
+
+}
+</style>
